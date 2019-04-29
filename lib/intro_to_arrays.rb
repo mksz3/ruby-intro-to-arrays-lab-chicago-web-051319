9c8cac976b4900d25bc253bc32e4e00a53d041b7
Markus Sanders <markus.sanderst@gmail.com>
2019-04-29 18:58:49 +0000

    Automatically backed up by Learn

--- lib/intro_to_arrays.rb
+++ lib/intro_to_arrays.rb
@@ -14,3 +14,19 @@ end
 def third_element(array)
   return array[2]
 end
+
+def last_element(array)
+  return array[array.length - 1]
+end
+
+def first_element_with_array_methods(array)
+  return array.first
+end
+
+def last_element_with_array_methods(array)
+  return array.last
+end
+
+def length_of_array(array)
+  return array.length
+end
