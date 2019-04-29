df535b7045ee7396d9c8a328598eb8f931a82922
Markus Sanders <markus.sanderst@gmail.com>
2019-04-29 18:56:10 +0000

    Automatically backed up by Learn

---
+++ lib/intro_to_arrays.rb
@@ -0,0 +1,16 @@
+def instantiate_new_array
+  return Array.new
+end
+
+def array_with_two_elements
+  arr = ["",""]
+  return arr
+end
+
+def first_element(array)
+  return array[0]
+end
+
+def third_element(array)
+  return array[2]
+end
